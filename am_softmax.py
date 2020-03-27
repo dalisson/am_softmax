559a6d5792839e90f06f36b50efa848c53cb62e6
Dalisson Figueiredo <dalissonfigueiredo@gmail.com>
2020-03-27 10:38:42 -0300

    Update am_softmax.py

--- am_softmax.py
+++ am_softmax.py
@@ -4,8 +4,10 @@ import torch.nn.functional as F
 
 class AMSoftmax(nn.Module):
     '''
-    the am softmax as seen on https://arxiv.org/pdf/1801.05599.pdf
-
+    the am softmax as seen on https://arxiv.org/pdf/1801.05599.pdf, expects a batch of embeddings as input.
+    inputs : tensor shaped batch_sizeXembedding_size eg. 64X512, 64 embeddings of size 512 each
+    outputs : softmax logits which can then be passed through a NLLoss layer
+    
     '''
     def __init__(self, in_features, n_classes, s, m):
         super(AMSoftmax, self).__init__()
