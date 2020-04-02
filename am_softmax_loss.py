4a30cedf5c5fb5c4cc3c8f45f19ef73fd2ef27c1
dalisson figueiredo <dalisson@mindsdigital.net>
2020-04-02 13:02:58 -0300

    am_softmax_loss refactored

--- am_softmax_loss.py
+++ am_softmax_loss.py
@@ -1,9 +1,8 @@
-import torch.nn as nn
 from torch.nn.functional import nll_loss
 from am_softmax import AMSoftmax
 
 
-class AMSoftmaxLoss(nn.Module):
+class AMSoftmaxLoss(AMSoftmax):
     '''
         AM softmax loss, computes NLL_Loss using AM_Softmax logits.
         in_features: embedding_size
@@ -14,11 +13,6 @@ class AMSoftmaxLoss(nn.Module):
         output: NLL_Loss computed using AM_softmax logits
     '''
 
-    def __init__(self, in_features, n_classes, s=30, m=0.4):
-        super(AMSoftmaxLoss, self).__init__()
-        self.am_softmax = AMSoftmax(in_features, n_classes, s, m)
-
-
     def forward(self, *inputs):
-        logits = self.am_softmax(inputs[0])
+        logits = super().forward(*inputs)
         return nll_loss(logits, inputs[1])
